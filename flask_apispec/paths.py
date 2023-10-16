1eb702a4f69942aa276048bae51b389ae9fc410c
Shawn McKenney <shawn.mckenney@emmion.com>
2023-10-16 16:42:07 -0500

    [IV-440]: Add a schema dict to hold param type info

--- flask_apispec/paths.py
+++ flask_apispec/paths.py
@@ -37,7 +37,9 @@ def argument_to_param(argument, rule, override=None):
         'required': True,
     }
     type_, format_ = CONVERTER_MAPPING.get(type(rule._converters[argument]), DEFAULT_TYPE)
-    param['type'] = type_
+    schema_type = dict()
+    schema_type['type'] = type_
+    param['schema'] = schema_type
     if format_ is not None:
         param['format'] = format_
     if rule.defaults and argument in rule.defaults:
